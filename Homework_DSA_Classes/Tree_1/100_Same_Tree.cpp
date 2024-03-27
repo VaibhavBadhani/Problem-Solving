7829465ad47b66b355e7c66ede54ab2e38cbf849
Vaibhav Badhani <badhanivaibhav@gmail.com>
2024-03-27 17:54:07 +0530

    100. Same Tree

---
+++ Homework_DSA_Classes/Tree_1/100_Same_Tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+          return isMirror(p, q);
+    }
+     bool isMirror(TreeNode* node1, TreeNode* node2) {
+        if (node1 == NULL && node2 == NULL) {
+            return true;
+        }
+        if (node1 == NULL || node2 == NULL) {
+            return false;
+        }
+        return node1->val == node2->val && isMirror(node1->left, node2->left) && isMirror(node1->right,node2->right);
+    }
+};
