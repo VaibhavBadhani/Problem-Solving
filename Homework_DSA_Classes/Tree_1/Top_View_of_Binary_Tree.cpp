9fd0779eb6a8fabed1e5c0a83a4406ea1f114828
Vaibhav Badhani <badhanivaibhav@gmail.com>
2024-03-27 18:16:20 +0530

    topView

---
+++ Homework_DSA_Classes/Tree_1/Top_View_of_Binary_Tree.cpp
@@ -0,0 +1,42 @@
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        if(!root) return vector<int>{};
+        map<int,int> mp;
+        queue<pair<Node*,pair<int,int>>>q;
+        q.push({root,{0,0}});
+        mp[0]=root->data;
+        while(!q.empty()){
+            auto ff = q.front();q.pop();
+            int x = ff.second.first;
+            int y = ff.second.second;
+            Node* nd = ff.first;
+            if(mp.find(x)==mp.end()){
+                mp[x] = nd->data;
+            }
+            if(nd->left){
+                q.push({nd->left,{x-1,y+1}});
+            }
+            if(nd->right){
+                q.push({nd->right,{x+1,y+1}});
+            }
+        }
+        vector<int>p;
+        for(auto a :mp){
+            p.push_back(a.second);
+        }
+        return p;
+    }
+
+};
