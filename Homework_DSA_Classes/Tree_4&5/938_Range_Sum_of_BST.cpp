1a403a092b0fb874051f2d0e658103310cfd8668
Vaibhav Badhani <badhanivaibhav@gmail.com>
2024-04-01 16:50:41 +0530

    Update 938_Range_Sum_of_BST.cpp

--- Homework_DSA_Classes/Tree_4&5/938_Range_Sum_of_BST.cpp
+++ Homework_DSA_Classes/Tree_4&5/938_Range_Sum_of_BST.cpp
@@ -11,22 +11,31 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, int low, int high,int& sum){
-        if(root==NULL) return;
+    // void solve(TreeNode* root, int low, int high,int& sum){
+    //     if(root==NULL) return;
+    //     if(root->val>=low&&root->val<=high){
+    //         sum+=root->val;
+    //     }
+    //     if(root->left){
+    //         solve(root->left,low,high,sum);
+    //     }
+    //     if(root->right){
+    //         solve(root->right,low,high,sum);
+    //     }
+    //     return;
+    // }
+    int bet(TreeNode* root, int low, int high){
+        if(!root) return 0;
+        int ls = bet(root->left,low,high);
+        int rs = bet(root->right,low,high);
         if(root->val>=low&&root->val<=high){
-            sum+=root->val;
+            return ls+rs+root->val;
         }
-        if(root->left){
-            solve(root->left,low,high,sum);
-        }
-        if(root->right){
-            solve(root->right,low,high,sum);
-        }
-        return;
+        return ls+rs;
     }
     int rangeSumBST(TreeNode* root, int low, int high) {
         int sum = 0;
-        solve(root,low,high,sum);
-        return sum;
+        // solve(root,low,high,sum);
+        return bet(root,low,high);
     }
 };
