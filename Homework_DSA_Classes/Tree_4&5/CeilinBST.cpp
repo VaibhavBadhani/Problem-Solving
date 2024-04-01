594c1aecb338abe9a30baeabde72eeac9c0bdc55
Vaibhav Badhani <badhanivaibhav@gmail.com>
2024-04-01 16:14:55 +0530

    Update CeilinBST.cpp

--- Homework_DSA_Classes/Tree_4&5/CeilinBST.cpp
+++ Homework_DSA_Classes/Tree_4&5/CeilinBST.cpp
@@ -15,9 +15,19 @@ void find(Node* root, int k,int& val){
     }
     return ;
 }
+int better(Node* root, int input){
+    if (root == NULL) return -1;
+    if(root->data== input) return input;
+    if(root->data<input){
+        return better(root->right,input);
+    }
+    int val = better(root->left, input);
+    return (val >= input) ? val : root->data;
+}
 int findCeil(Node* root, int input) {
     if (root == NULL) return -1;
-    int val =-1;
-    find(root,input,val); 
-    return val;
+    // int val =-1;
+    // find(root,input,val); 
+    // return val;
+    return better(root,input);
 }
